8fa0434a4066f97ca9337352df4021ca74051b94
AoEiuV020 <aoeiuv020@gmail.com>
2021-06-24 09:00:02 +0800

    ninja chrome/gpu

---
+++ f/0/1db1800002665c73c5f9fed98a2d32-2869686.d
@@ -0,0 +1,130 @@
+obj/content/public/gpu/gpu_sources/content_gpu_client.o: \
+  ../../content/public/gpu/content_gpu_client.cc \
+  ../../content/public/gpu/content_gpu_client.h \
+  ../../base/metrics/field_trial.h ../../base/atomicops.h \
+  ../../base/base_export.h ../../build/build_config.h \
+  ../../base/atomicops_internals_portable.h ../../base/command_line.h \
+  ../../base/strings/string16.h ../../base/strings/string_piece.h \
+  ../../base/logging.h ../../base/callback_forward.h \
+  ../../base/compiler_specific.h ../../base/debug/debugger.h \
+  ../../base/macros.h ../../base/strings/string_piece_forward.h \
+  ../../base/template_util.h ../../base/strings/char_traits.h \
+  ../../base/feature_list.h ../../base/gtest_prod_util.h \
+  ../../testing/gtest/include/gtest/gtest_prod.h \
+  ../../third_party/googletest/src/googletest/include/gtest/gtest_prod.h \
+  ../../base/metrics/persistent_memory_allocator.h \
+  ../../base/files/file_path.h ../../base/synchronization/lock.h \
+  ../../base/synchronization/lock_impl.h \
+  ../../base/threading/platform_thread.h ../../base/time/time.h \
+  ../../base/numerics/safe_math.h ../../base/numerics/checked_math.h \
+  ../../base/numerics/checked_math_impl.h \
+  ../../base/numerics/safe_conversions.h \
+  ../../base/numerics/safe_conversions_impl.h \
+  ../../base/numerics/safe_math_shared_impl.h \
+  ../../base/numerics/safe_math_clang_gcc_impl.h \
+  ../../base/numerics/clamped_math.h \
+  ../../base/numerics/clamped_math_impl.h ../../base/files/file.h \
+  ../../base/files/file_tracing.h ../../base/files/platform_file.h \
+  ../../base/files/scoped_file.h ../../base/scoped_generic.h \
+  ../../base/memory/ref_counted.h ../../base/atomic_ref_count.h \
+  ../../base/memory/scoped_refptr.h ../../base/sequence_checker.h \
+  ../../base/sequence_checker_impl.h \
+  ../../base/threading/thread_collision_warner.h \
+  ../../base/memory/shared_memory.h ../../base/hash.h \
+  ../../base/memory/shared_memory_handle.h \
+  ../../base/unguessable_token.h ../../base/file_descriptor_posix.h \
+  ../../base/process/process_handle.h ../../base/files/file_util.h \
+  ../../base/posix/eintr_wrapper.h ../../base/observer_list_threadsafe.h \
+  ../../base/bind.h ../../base/bind_internal.h \
+  ../../base/callback_internal.h \
+  ../../base/memory/raw_scoped_refptr_mismatch_checker.h \
+  ../../base/memory/weak_ptr.h ../../base/lazy_instance.h \
+  ../../base/debug/leak_annotations.h ../../base/lazy_instance_helpers.h \
+  ../../base/threading/thread_restrictions.h ../../base/location.h \
+  gen/base/debug/debugging_buildflags.h ../../build/buildflag.h \
+  ../../base/observer_list.h ../../base/stl_util.h ../../base/optional.h \
+  ../../base/sequenced_task_runner.h ../../base/callback.h \
+  ../../base/sequenced_task_runner_helpers.h ../../base/task_runner.h \
+  ../../base/threading/sequenced_task_runner_handle.h \
+  ../../base/threading/thread_local.h \
+  ../../base/threading/thread_local_storage.h \
+  ../../base/memory/ptr_util.h ../../base/single_thread_task_runner.h \
+  ../../base/threading/thread_task_runner_handle.h \
+  ../../base/callback_helpers.h ../../base/pickle.h \
+  ../../base/process/launch.h ../../base/environment.h \
+  ../../base/process/process.h \
+  ../../base/posix/file_descriptor_shuffle.h \
+  ../../content/public/common/content_client.h \
+  ../../content/common/content_export.h ../../ui/base/layout.h \
+  ../../ui/base/resource/scale_factor.h \
+  ../../ui/base/resource/data_pack_export.h \
+  ../../ui/base/ui_base_export.h ../../ui/gfx/native_widget_types.h \
+  ../../base/android/scoped_java_ref.h ../../url/gurl.h \
+  ../../base/debug/alias.h ../../base/strings/string_util.h \
+  ../../base/strings/string_util_posix.h \
+  ../../url/third_party/mozilla/url_parse.h ../../url/url_export.h \
+  ../../url/url_canon.h ../../base/export_template.h \
+  ../../url/url_canon_stdstring.h ../../url/url_constants.h \
+  ../../url/origin.h ../../url/scheme_host_port.h ../../url/url_util.h \
+  gen/media/media_buildflags.h \
+  ../../services/service_manager/public/cpp/binder_registry.h \
+  ../../mojo/public/cpp/system/message_pipe.h \
+  ../../mojo/public/c/system/message_pipe.h \
+  ../../mojo/public/c/system/macros.h \
+  ../../mojo/public/c/system/system_export.h \
+  ../../mojo/public/c/system/types.h \
+  ../../mojo/public/cpp/system/handle.h \
+  ../../mojo/public/c/system/functions.h \
+  ../../mojo/public/cpp/system/handle_signals_state.h \
+  ../../mojo/public/cpp/system/system_export.h \
+  ../../mojo/public/cpp/system/message.h \
+  ../../services/service_manager/public/cpp/export.h \
+  ../../services/service_manager/public/cpp/interface_binder.h \
+  ../../mojo/public/cpp/bindings/interface_request.h \
+  ../../mojo/public/cpp/bindings/disconnect_reason.h \
+  ../../mojo/public/cpp/bindings/interface_ptr.h \
+  ../../mojo/public/cpp/bindings/connection_error_callback.h \
+  ../../mojo/public/cpp/bindings/interface_ptr_info.h \
+  ../../mojo/public/cpp/bindings/lib/interface_ptr_state.h \
+  ../../mojo/public/cpp/bindings/associated_group.h \
+  ../../base/component_export.h \
+  ../../mojo/public/cpp/bindings/scoped_interface_endpoint_handle.h \
+  ../../mojo/public/cpp/bindings/interface_id.h \
+  ../../mojo/public/cpp/bindings/bindings_export.h \
+  ../../mojo/public/cpp/bindings/filter_chain.h \
+  ../../mojo/public/cpp/bindings/message.h \
+  ../../mojo/public/cpp/bindings/lib/buffer.h \
+  ../../mojo/public/cpp/bindings/lib/message_internal.h \
+  ../../mojo/public/cpp/bindings/lib/bindings_internal.h \
+  ../../mojo/public/cpp/bindings/enum_traits.h \
+  ../../mojo/public/cpp/bindings/lib/template_util.h \
+  ../../mojo/public/cpp/system/core.h \
+  ../../mojo/public/cpp/system/buffer.h \
+  ../../mojo/public/c/system/buffer.h \
+  ../../mojo/public/cpp/system/data_pipe.h \
+  ../../mojo/public/c/system/data_pipe.h \
+  ../../mojo/public/cpp/system/functions.h \
+  ../../mojo/public/cpp/bindings/lib/unserialized_message_context.h \
+  ../../mojo/public/cpp/bindings/lib/serialization_context.h \
+  ../../base/containers/stack_container.h \
+  ../../mojo/public/cpp/bindings/interface_endpoint_client.h \
+  ../../mojo/public/cpp/bindings/lib/control_message_handler.h \
+  ../../mojo/public/cpp/bindings/lib/control_message_proxy.h \
+  ../../mojo/public/cpp/bindings/lib/multiplex_router.h \
+  ../../base/containers/queue.h ../../base/containers/circular_deque.h \
+  ../../base/containers/vector_buffer.h \
+  ../../base/containers/small_map.h ../../base/containers/hash_tables.h \
+  ../../mojo/public/cpp/bindings/associated_group_controller.h \
+  ../../mojo/public/cpp/bindings/connector.h \
+  ../../mojo/public/cpp/bindings/sync_handle_watcher.h \
+  ../../mojo/public/cpp/bindings/sync_handle_registry.h \
+  ../../base/synchronization/waitable_event.h \
+  ../../mojo/public/cpp/system/wait_set.h \
+  ../../mojo/public/c/system/trap.h \
+  ../../mojo/public/cpp/system/handle_signal_tracker.h \
+  ../../mojo/public/cpp/system/simple_watcher.h \
+  ../../mojo/public/cpp/system/trap.h \
+  ../../mojo/public/cpp/bindings/message_header_validator.h \
+  ../../mojo/public/cpp/bindings/pipe_control_message_handler.h \
+  ../../mojo/public/cpp/bindings/pipe_control_message_handler_delegate.h \
+  ../../mojo/public/cpp/bindings/pipe_control_message_proxy.h
