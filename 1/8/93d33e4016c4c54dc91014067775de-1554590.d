ae2e03b8d3404aa8de0d40f67fa4951ac158a983
AoEiuV020 <aoeiuv020@gmail.com>
2021-10-15 14:42:00 +0800

    ninja components/guest_view/renderer

---
+++ 1/8/93d33e4016c4c54dc91014067775de-1554590.d
@@ -0,0 +1,11 @@
+obj/components/version_info/version_info/version_info.o: \
+  ../../components/version_info/version_info.cc \
+  ../../components/version_info/version_info.h \
+  ../../components/version_info/channel.h ../../base/logging.h \
+  ../../base/base_export.h ../../base/callback_forward.h \
+  ../../base/compiler_specific.h ../../build/build_config.h \
+  ../../base/debug/debugger.h ../../base/macros.h \
+  ../../base/strings/string_piece_forward.h \
+  ../../base/strings/string16.h ../../base/template_util.h \
+  ../../base/no_destructor.h ../../base/version.h \
+  gen/components/version_info/version_info_values.h
